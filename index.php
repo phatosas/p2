d9142cadc8f6cc1e60138c8e1ea28d6651567cab
phatosas <phatosas@yahoo.com>
2016-02-25 03:01:51 -0500

    Adding index, logic and css files, initial stage of building the website

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+phpinfo();
+?>
+
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>xkcd passoword generator</title>
+	<meta charset='utf-8'>
+
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+	<link href='//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css' rel="stylesheet">
+
+	<link href='mywebsite.css' rel='stylesheet'>
+
+</head>
+
+<body>
+<p>
+	<form>
+	<input type="checkbox" name="vehicle1" value="Bike"> I have a bike<br>
+	<input type="checkbox" name="vehicle2" value="Car"> I have a car
+	</form> 
+	</p>
+	<p>
+    <img class = "comic" src='http://imgs.xkcd.com/comics/password_strength.png ' alt='xkcd password generator comic'>
+	</p>
+
+</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+Create an array of words dependent on the password length
+The words are pulled up from the array using a random method to form a password
+*/
+
+
