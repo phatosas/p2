e77c231603106d30f7cee11a9cd312f70575cbf9
phatosas <phatosas@yahoo.com>
2016-03-05 21:10:34 -0500

    updating index file, style file and images

--- index.php
+++ index.php
@@ -6,42 +6,40 @@ ini_set('display_errors', 1);
 <!DOCTYPE html>
 <html>
 <head>
-	<title>xkcd passoword generator</title>
+	<title>xkcd password generator</title>
 	<meta charset='utf-8'>
-
-	<link rel="stylesheet" media="screen" href="/214/214.css?v=8may2013">
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="http://www.csszengarden.com/zengarden.xml">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="author" content="Dave Shea">
-	<meta name="description" content="A demonstration of what can be accomplished visually through CSS-based design.">
-	<meta name="robots" content="all">
-
-
-	<!--[if lt IE 9]>
-	<script src="script/html5shiv.js"></script>
-	<![endif]-->
+	<link rel='stylesheet' href='style.css'>
 </head>
 
+<header class="main-header" role="banner">
+	<img src="images/mybannerimage.jpg" alt="Banner Image"/>
+</header>
 <body>
-	Select the number of words (four by default)<br>
-	<select>
-    <option value="four"> </option>
-    <option value="One">One</option>
-    <option value="Two">Two</option>
-    <option value="Three">Three</option>
-	<option value="Four">Four</option>
-    <option value="Five">Five</option>
-    <option value="Six">Six</option>
-    <option value="Seven">Seven</option>
-	<option value="Eight">Eight</option>
-    <option value="Nine">Nine</option>
-    </select> 
+	<h1>xkcd password generator</h1>
+	<p><input type="button" id="generate_password" name="generate_password" value="Generate Password" />
+	
+	<p class='password'> onto-job-shade-hunt </p>
+	<p>
+	Select the number of words to make up the password<br>
+		<select>
+    		<option value="One">One</option>
+    		<option value="Two">Two</option>
+    		<option value="Three">Three</option>
+			<option value="Four" selected='selected'>Four</option>
+			<option value="Five">Five</option>
+    		<option value="Six">Six</option>
+    		<option value="Seven">Seven</option>
+			<option value="Eight">Eight</option>
+    		<option value="Nine">Nine</option>
+    	</select>
+	</p> 
 	<form>
-	<input type="checkbox" name="vehicle1" value="Bike"> Include Special Character<br>
-	<input type="checkbox" name="vehicle2" value="Car"> Include a Number
+	<p>
+		<input type="checkbox" name="vehicle1" value="Bike"> Include Special Character<br>
+		<input type="checkbox" name="vehicle2" value="Car"> Include a Number
+	</p>	
 	</form> 
-
+	<h2>The Comic that Inspired This Tool</h2>
 	<p>
         <img class = "comic" src='http://imgs.xkcd.com/comics/password_strength.png ' alt='xkcd 
 password generator comic'>
