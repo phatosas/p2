56daca773584dcb32bd760088a08e0c8cae052c3
phatosas <phatosas@yahoo.com>
2016-03-07 16:24:10 -0500

    updating index.php logic.php style.css

--- index.php
+++ index.php
@@ -1,47 +1,52 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>xkcd password generator</title>
 	<meta charset='utf-8'>
 	<link rel='stylesheet' href='style.css'>
+
+	<?php require('logic.php') ?>
 </head>
 
 <header class="main-header" role="banner">
 	<img src="images/mybannerimage.jpg" alt="Banner Image"/>
 </header>
 <body>
-	<h1>xkcd password generator</h1>
-	<p><input type="button" id="generate_password" name="generate_password" value="Generate Password" />
+	<form action='index.php' method='POST'>
+		<div>
+			<h1>xkcd password generator</h1>
+			<p>
+				<input type= 'submit' value='Generate Password'>
+			</p>
+			<p class='password'> <?php echo $password ?> </p>
+		</div>
+		<div>
+			<h3>Enter the number of words</h3>
+			<p>
+				<input type='text' name='num_words' value=''>  (Max 9)
+				<br>
+			</p>
+		</div>
+		<div>
+			<h3>Select a delimeter</h3>
+			<p>	
+				<input type="radio" name="delimeter" value="space" checked> Space<br>
+				<input type="radio" name="delimeter" value="camelcase"> CamelCase<br>
+				<input type="radio" name="delimeter" value="hyphens"> Hyphens<br>
+			</P>
+		</div>
+		<div>
+			<h3>Other Options</h3>
+			<p>
+				<input type='checkbox' autocomplete="off" name='add_number' value='add_number'> Add a number<br>
+				<input type='checkbox' autocomplete="off" name='add_symbol' value='add_symbol'>Add a symbol<br>
+			</p>
+		</div>
+	</form>
 	
-	<p class='password'> onto-job-shade-hunt </p>
-	<p>
-	Select the number of words to make up the password<br>
-		<select>
-    		<option value="One">One</option>
-    		<option value="Two">Two</option>
-    		<option value="Three">Three</option>
-			<option value="Four" selected='selected'>Four</option>
-			<option value="Five">Five</option>
-    		<option value="Six">Six</option>
-    		<option value="Seven">Seven</option>
-			<option value="Eight">Eight</option>
-    		<option value="Nine">Nine</option>
-    	</select>
-	</p> 
-	<form>
-	<p>
-		<input type="checkbox" name="vehicle1" value="Bike"> Include Special Character<br>
-		<input type="checkbox" name="vehicle2" value="Car"> Include a Number
-	</p>	
-	</form> 
 	<h2>The Comic that Inspired This Tool</h2>
 	<p>
-        <img class = "comic" src='http://imgs.xkcd.com/comics/password_strength.png ' alt='xkcd 
+        	<img class = "comic" src='http://imgs.xkcd.com/comics/password_strength.png ' alt='xkcd 
 password generator comic'>
 	</p>
 
--- logic.php
+++ logic.php
@@ -1,8 +1,50 @@
 <?php
-
 /*
 Create an array of words dependent on the password length
 The words are pulled up from the array using a random method to form a password
 */
 
+$word_list = array("about", "below", "came", "day", "end", "find", "give", "have", "into", "just", 
+"know", "large", "made", "never", "odd", "parcel", "quote", "rate", "think", "umpire", "vex", "wind", 
+"xray", "you", "zoom");
 
+if ($_POST["num_words"] == ""){
+	$num_words = 4;
+} else {
+	$num_words = $_POST["num_words"];
+}
+$rand_keys = array_rand($word_list, $num_words);
+$password = '';
+$i = 0;
+$random_char = array('!', '@', '#', '$', '%', '&', '*', '?');
+
+if ($_POST["delimeter"] == "space"){
+	while ($i < $num_words){
+		$password .= $word_list[$rand_keys[$i]]. " ";
+		$i++;
+	}
+}
+else if ($_POST["delimeter"] == "camelcase"){
+	while ($i < $num_words){
+		$password .= ucfirst($word_list[$rand_keys[$i]]);
+		$i++;
+	}
+}
+else if ($_POST["delimeter"] == "hyphens"){
+	$password = $word_list[$rand_keys[0]];
+	$i++;
+	while ($i < $num_words){
+		$password .= "-". $word_list[$rand_keys[$i]];
+		$i++;
+	}
+}
+if (array_key_exists("add_number", $_POST )){
+	$number = rand( 0 , 9 );
+	$password .= $number;
+}
+if (array_key_exists("add_symbol", $_POST )){
+	$index = rand( 0 , 7 );
+	$password .= $random_char[$index];
+}
+
+?>
